61715ff5d4993d015c8dd6ca5d07623414eac65f
Ivorra-1996 <Ivorra-1996>
2024-12-19 11:36:43 -0300

    fixes

--- ejecutar.py
+++ ejecutar.py
@@ -3,15 +3,27 @@ from ultralytics import YOLO
 
 # Configuración
 MODEL_PATH = "./runs/detect/train/weights/best.pt"  # Modelo entrenado
-CAMERA_URL = "http://192.168.0.82:4747/video"  # URL de DroidCamApp
+CAMERA_URL = "http://192.168.0.69:4747/video"  # URL de DroidCamApp
+
+# Configuración de la resolución (640x640 por defecto en YOLO)
+FRAME_WIDTH = 640
+FRAME_HEIGHT = 640
 
 def run_inference():
+    frame_skip : int = 4  # Procesa un cuadro cada 4
+    frame_count : int = 0
+
     print("Cargando modelo YOLO entrenado...")
     model = YOLO(MODEL_PATH)
     print("Tipo de clase: ", model.names)
 
     print("Conectando a la cámara...")
     cap = cv2.VideoCapture(CAMERA_URL)
+
+    print("Configurando resolución...")
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, FRAME_WIDTH)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
+
 
     if not cap.isOpened():
         print("Error: No se puede conectar a la cámara.")
@@ -23,12 +35,13 @@ def run_inference():
         if not ret:
             break
 
-        # Realizar inferencia
-        results = model(frame, imgsz=640)  # Fuerza la resolución a 640x640
-        
-        # Mostrar resultados en pantalla
-        annotated_frame = results[0].plot()
-        cv2.imshow("Detección en tiempo real", annotated_frame)
+        # Salta cuadros 
+        if frame_count % frame_skip == 0:
+            results = model(frame)
+            annotated_frame = results[0].plot()
+            cv2.imshow("Detección en tiempo real", annotated_frame)
+
+        frame_count += 1
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
